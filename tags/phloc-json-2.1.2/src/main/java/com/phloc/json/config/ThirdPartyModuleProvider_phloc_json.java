4b540b711ffd92ca85dd20096981b35ea8f1cebb
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2012-03-14 17:52:39 +0000

    [maven-release-plugin]  copy for tag phloc-json-2.1.2

---
+++ tags/phloc-json-2.1.2/src/main/java/com/phloc/json/config/ThirdPartyModuleProvider_phloc_json.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json.config;
+
+import javax.annotation.Nullable;
+
+import com.phloc.commons.annotations.IsSPIImplementation;
+import com.phloc.commons.thirdparty.ELicense;
+import com.phloc.commons.thirdparty.IThirdPartyModule;
+import com.phloc.commons.thirdparty.IThirdPartyModuleProviderSPI;
+import com.phloc.commons.thirdparty.ThirdPartyModule;
+import com.phloc.commons.version.Version;
+
+/**
+ * Implement this SPI interface if your JAR file contains external third party
+ * modules.
+ * 
+ * @author philip
+ */
+@IsSPIImplementation
+public final class ThirdPartyModuleProvider_phloc_json implements IThirdPartyModuleProviderSPI
+{
+  private static final IThirdPartyModule JACKSON = new ThirdPartyModule ("Jackson",
+                                                                         "codehaus",
+                                                                         ELicense.APACHE2,
+                                                                         new Version (1, 9, 5),
+                                                                         "http://jackson.codehaus.org/");
+
+  @Nullable
+  public IThirdPartyModule [] getAllThirdPartyModules ()
+  {
+    return new IThirdPartyModule [] { JACKSON };
+  }
+}
