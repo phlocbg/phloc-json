182efa4c74d53a6081d7c25a87327de8c3a7f5c9
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2012-03-26 09:46:27 +0000

    [maven-release-plugin]  copy for tag phloc-json-2.1.6

---
+++ tags/phloc-json-2.1.6/src/main/java/com/phloc/json/impl/value/JSONPropertyValueBoolean.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json.impl.value;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.phloc.json.IJSONPropertyValue;
+
+/**
+ * Implementation of {@link IJSONPropertyValue} with the internal data type
+ * {@link Boolean}
+ * 
+ * @author Boris Gregorcic
+ */
+public class JSONPropertyValueBoolean extends AbstractJSONPropertyValue <Boolean>
+{
+  /**
+   * Ctor
+   * 
+   * @param aData
+   */
+  public JSONPropertyValueBoolean (@Nullable final Boolean aData)
+  {
+    super (aData);
+  }
+
+  /**
+   * Ctor
+   * 
+   * @param bData
+   */
+  public JSONPropertyValueBoolean (final boolean bData)
+  {
+    this (Boolean.valueOf (bData));
+  }
+
+  public void appendJSONString (final StringBuilder aResult, final boolean bAlignAndIndent, final int nLevel)
+  {
+    aResult.append (Boolean.toString (getData ().booleanValue ()));
+  }
+
+  @Nonnull
+  public JSONPropertyValueBoolean getClone ()
+  {
+    return new JSONPropertyValueBoolean (getData ());
+  }
+
+  /**
+   * Tries to create a {@link JSONPropertyValueBoolean} from the passed JSON
+   * string
+   * 
+   * @param sJSON
+   *        the JSON string to convert
+   * @return the resulting object
+   */
+  @Nonnull
+  public static JSONPropertyValueBoolean fromJSONString (final String sJSON)
+  {
+    return new JSONPropertyValueBoolean (Boolean.parseBoolean (sJSON));
+  }
+}
