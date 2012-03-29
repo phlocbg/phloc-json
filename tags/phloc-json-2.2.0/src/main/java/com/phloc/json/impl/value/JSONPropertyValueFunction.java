d2b2be28e7bef71a3f2795542e80e03ed7a9ef17
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2012-03-29 03:58:29 +0000

    [maven-release-plugin]  copy for tag phloc-json-2.2.0

---
+++ tags/phloc-json-2.2.0/src/main/java/com/phloc/json/impl/value/JSONPropertyValueFunction.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json.impl.value;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.json.IJSONPropertyValue;
+import com.phloc.json.impl.JSONReader;
+
+/**
+ * Implementation of {@link IJSONPropertyValue} with the internal data type
+ * {@link String}<br>
+ * <br>
+ * <b>ATTENTION:</b> keyword properties cannot be parsed again due to the fact,
+ * that the {@link JSONReader#parseObject(String)} uses the default
+ * {@link com.fasterxml.jackson.databind.ObjectMapper} which is not capable to
+ * handle keywords.
+ * 
+ * @author Boris Gregorcic
+ */
+public class JSONPropertyValueFunction extends AbstractJSONPropertyValue <String>
+{
+  private final String m_sBody;
+  private final String [] m_aParams;
+
+  /**
+   * Ctor
+   * 
+   * @param sBody
+   *        The function body (executed code)
+   * @param aParams
+   *        The parameter arguments this function takes
+   */
+  public JSONPropertyValueFunction (@Nonnull final String sBody, @Nonnull final String... aParams)
+  {
+    super (_getFunctionCode (sBody, aParams));
+    m_sBody = sBody;
+    m_aParams = aParams;
+  }
+
+  @Nonnull
+  private static String _getFunctionCode (@Nonnull final String sBody, @Nonnull final String... aParams)
+  {
+    final StringBuilder aData = new StringBuilder ("function(");
+    boolean bFirst = true;
+    for (final String sParam : aParams)
+    {
+      if (!bFirst)
+        aData.append (",");
+      aData.append (sParam);
+      bFirst = false;
+    }
+    aData.append ("){").append (sBody).append ("}");
+    return aData.toString ();
+  }
+
+  public void appendJSONString (final StringBuilder aResult, final boolean bAlignAndIndent, final int nLevel)
+  {
+    aResult.append (getData ());
+  }
+
+  @Nonnull
+  public JSONPropertyValueFunction getClone ()
+  {
+    return new JSONPropertyValueFunction (m_sBody, m_aParams);
+  }
+}
---
+++ tags/phloc-json-2.2.0/src/test/java/com/phloc/json/JSONComplexUtilsTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.phloc.json.IJSONObject;
+import com.phloc.json.JSONComplexUtils;
+import com.phloc.json.impl.CJSONConstants;
+import com.phloc.json.impl.value.JSONPropertyValueKeyword;
+
+public final class JSONComplexUtilsTest
+{
+  private static final String PROP1 = "Property1";
+  private static final String PROP2 = "Property2";
+  private static final String PROP3 = "Property3";
+  private static final String PROP4 = "Property4";
+  private static final String PROP5 = "Property5";
+
+  private static final String VALUE_STRING = "VALUE";
+  private static final Integer VALUE_INTEGER = Integer.valueOf (42);
+  private static final Double VALUE_DOUBLE = Double.valueOf (3.2);
+  private static final Boolean VALUE_BOOLEAN = Boolean.FALSE;
+
+  @Test
+  public void testConvertToJSON ()
+  {
+    // test empty (null) property map
+    {
+      final IJSONObject aObj = JSONComplexUtils.convertToJSON (null);
+      assertNotNull (aObj);
+      assertEquals (aObj.getAllPropertyNames ().size (), 0);
+    }
+    // test mixed types
+    {
+      final Map <String, Object> aProperties = new HashMap <String, Object> ();
+      aProperties.put (PROP1, VALUE_STRING);
+      aProperties.put (PROP2, VALUE_INTEGER);
+      aProperties.put (PROP3, VALUE_DOUBLE);
+      aProperties.put (PROP4, VALUE_BOOLEAN);
+      aProperties.put (PROP5, null);
+      final IJSONObject aObj = JSONComplexUtils.convertToJSON (aProperties);
+      assertNotNull (aObj);
+      assertEquals (aObj.getAllPropertyNames ().size (), aProperties.size ());
+      assertEquals (aObj.getProperty (PROP1).getValue ().getData (), VALUE_STRING);
+      assertEquals (aObj.getProperty (PROP2).getValue ().getData (), VALUE_INTEGER);
+      assertEquals (aObj.getProperty (PROP3).getValue ().getData (), VALUE_DOUBLE);
+      assertEquals (aObj.getProperty (PROP4).getValue ().getData (), VALUE_BOOLEAN);
+      assertTrue (aObj.getProperty (PROP5).getValue () instanceof JSONPropertyValueKeyword);
+      assertEquals (aObj.getProperty (PROP5).getValue ().getData (), CJSONConstants.KEYWORD_NULL);
+    }
+  }
+}
