d42239c214b7c8847ceb88964d0fe9cae48b4285
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2012-05-16 05:59:41 +0000

    [maven-release-plugin]  copy for tag phloc-json-2.2.7

---
+++ tags/phloc-json-2.2.7/src/main/java/com/phloc/json/JSONComplexUtils.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json;
+
+import java.util.Map;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.concurrent.Immutable;
+
+import com.phloc.commons.collections.ContainerHelper;
+import com.phloc.json.impl.CJSONConstants;
+import com.phloc.json.impl.JSONObject;
+
+/**
+ * General utility class for JSON string operations like escaping and unescaping
+ * 
+ * @author Boris Gregorcic
+ */
+@Immutable
+public final class JSONComplexUtils
+{
+  private JSONComplexUtils ()
+  {}
+
+  /**
+   * Converts the passed property map to a new JSON object containing all
+   * properties from the map.<br/>
+   * <b>ATTENTION:</b> Currently only simple types are supported, no container
+   * types or nested objects!
+   * 
+   * @param aProperties
+   * @return the created JSON object
+   */
+  public static IJSONObject convertToJSON (@CheckForNull final Map <String, Object> aProperties)
+  {
+    return convertToJSON (aProperties, false);
+  }
+
+  /**
+   * Converts the passed property map to a new JSON object containing all
+   * properties from the map.<br/>
+   * <b>ATTENTION:</b> Currently only simple types are supported, no container
+   * types or nested objects!
+   * 
+   * @param aProperties
+   *        The source map
+   * @param bUseTypeConverter
+   *        If <code>true</code> the type converter registry should be used to
+   *        convert simple values.
+   * @return the created JSON object
+   */
+  public static IJSONObject convertToJSON (@CheckForNull final Map <String, Object> aProperties,
+                                           final boolean bUseTypeConverter)
+  {
+    final IJSONObject aJSON = new JSONObject ();
+    if (aProperties != null)
+    {
+      // Sort map by key, to create reproducible results
+      for (final Map.Entry <String, Object> aEntry : ContainerHelper.getSortedByKey (aProperties).entrySet ())
+      {
+        final String sPropertyName = aEntry.getKey ();
+        final Object aValue = aEntry.getValue ();
+        if (aValue == null)
+          aJSON.setKeywordProperty (sPropertyName, CJSONConstants.KEYWORD_NULL);
+        else
+          if (aValue instanceof IJSONConvertible)
+          {
+            final IJSONObject aNestedValue = ((IJSONConvertible) aValue).getAsJSON ();
+            aJSON.setObjectProperty (sPropertyName, aNestedValue);
+          }
+          else
+            if (aValue instanceof Map <?, ?>)
+            {
+              @SuppressWarnings ("unchecked")
+              final IJSONObject aNestedValue = convertToJSON ((Map <String, Object>) aValue);
+              aJSON.setObjectProperty (sPropertyName, aNestedValue);
+            }
+            else
+              aJSON.setProperty (sPropertyName, aValue, bUseTypeConverter);
+      }
+    }
+    return aJSON;
+  }
+}
