d97f20ec3f1c4dead97fe234be6a26ae0d3a1a32
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2012-09-06 10:58:05 +0000

    Updated to Jackson 2.0.6

--- trunk/src/main/java/com/phloc/json/config/ThirdPartyModuleProvider_phloc_json.java
+++ trunk/src/main/java/com/phloc/json/config/ThirdPartyModuleProvider_phloc_json.java
@@ -38,7 +38,7 @@ public final class ThirdPartyModuleProvider_phloc_json implements IThirdPartyMod
   private static final IThirdPartyModule JACKSON = new ThirdPartyModule ("Jackson",
                                                                          "codehaus",
                                                                          ELicense.APACHE2,
-                                                                         new Version (2, 0, 5),
+                                                                         new Version (2, 0, 6),
                                                                          "http://wiki.fasterxml.com/JacksonHome");
 
   @Nullable
