39648c294735c74729b8e10d665a989150fe22c0
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2012-09-06 15:30:49 +0000

    [maven-release-plugin]  copy for tag phloc-json-2.3.5

---
+++ tags/phloc-json-2.3.5/src/main/java/com/phloc/json/IJSONPropertyValue.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Interface for a JSON property value. The internal data type can be specified
+ * via the template parameter
+ * 
+ * @author Boris Gregorcic
+ * @param <DATATYPE>
+ *        the internal data type of the value
+ */
+public interface IJSONPropertyValue <DATATYPE> extends IJSON
+{
+  /**
+   * @return the internal data value
+   */
+  @Nonnull
+  DATATYPE getData ();
+
+  /**
+   * Sets the passed data value
+   * 
+   * @param aValue
+   *        the value to set
+   */
+  @Deprecated
+  void setData (@Nonnull DATATYPE aValue);
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  IJSONPropertyValue <DATATYPE> getClone ();
+}
---
+++ tags/phloc-json-2.3.5/src/main/java/com/phloc/json/impl/value/JSONPropertyValueKeyword.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2006-2012 phloc systems
+ * http://www.phloc.com
+ * office[at]phloc[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phloc.json.impl.value;
+
+import javax.annotation.Nonnull;
+
+import com.phloc.json.IJSONPropertyValue;
+import com.phloc.json.IJSONPropertyValueNotParsable;
+import com.phloc.json.JSONHelper;
+import com.phloc.json.impl.JSONReader;
+
+/**
+ * Implementation of {@link IJSONPropertyValue} with the internal data type
+ * {@link String}<br>
+ * <br>
+ * <b>ATTENTION:</b> keyword properties cannot be parsed again due to the fact,
+ * that the {@link JSONReader#parseObject(String)} uses the default
+ * {@link com.fasterxml.jackson.databind.ObjectMapper} which is not capable to
+ * handle keywords.
+ * 
+ * @author Boris Gregorcic
+ */
+public class JSONPropertyValueKeyword extends AbstractJSONPropertyValue <String> implements
+                                                                                IJSONPropertyValueNotParsable <String>
+{
+  /**
+   * Ctor
+   * 
+   * @param sData
+   */
+  public JSONPropertyValueKeyword (@Nonnull final String sData)
+  {
+    super (sData);
+  }
+
+  public void appendJSONString (@Nonnull final StringBuilder aResult, final boolean bAlignAndIndent, final int nLevel)
+  {
+    aResult.append (JSONHelper.jsonEscape (getData ()));
+  }
+
+  @Nonnull
+  public JSONPropertyValueKeyword getClone ()
+  {
+    return new JSONPropertyValueKeyword (getData ());
+  }
+}
