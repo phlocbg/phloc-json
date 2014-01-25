9d320309ef9beb27eab2de29943d22f6bc9f912c
ph <ph@phloc.com>
2014-01-25 21:04:50 +0000

    Changed default behaviour of escaper to used escaping and instead using the numeric version only for the numeric types

--- phloc-json2/src/main/java/com/phloc/json2/serialize/JsonValueSerializerRegistry.java
+++ phloc-json2/src/main/java/com/phloc/json2/serialize/JsonValueSerializerRegistry.java
@@ -17,8 +17,13 @@
  */
 package com.phloc.json2.serialize;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -27,6 +32,11 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.phloc.commons.annotations.Nonempty;
+import com.phloc.commons.lang.GenericReflection;
 import com.phloc.json2.IJsonValueSerializer;
 
 /**
@@ -38,6 +48,7 @@ import com.phloc.json2.IJsonValueSerializer;
 @ThreadSafe
 public final class JsonValueSerializerRegistry
 {
+  private static final Logger s_aLogger = LoggerFactory.getLogger (JsonValueSerializerRegistry.class);
   private static final JsonValueSerializerRegistry s_aInstance = new JsonValueSerializerRegistry ();
   private static final ReadWriteLock s_aRWLock = new ReentrantReadWriteLock ();
 
@@ -46,10 +57,18 @@ public final class JsonValueSerializerRegistry
 
   static
   {
-    getInstance ().registerJsonValueSerializer (CharSequence.class, JsonValueSerializerEscaped.getInstance ());
-    getInstance ().registerJsonValueSerializer (String.class, JsonValueSerializerEscaped.getInstance ());
-    getInstance ().registerJsonValueSerializer (StringBuilder.class, JsonValueSerializerEscaped.getInstance ());
-    getInstance ().registerJsonValueSerializer (StringBuffer.class, JsonValueSerializerEscaped.getInstance ());
+    getInstance ().registerJsonValueSerializer (AtomicBoolean.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (AtomicInteger.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (AtomicLong.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Boolean.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Byte.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Double.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Float.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Integer.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Long.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (Short.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (BigDecimal.class, JsonValueSerializerToString.getInstance ());
+    getInstance ().registerJsonValueSerializer (BigInteger.class, JsonValueSerializerToString.getInstance ());
   }
 
   private JsonValueSerializerRegistry ()
@@ -62,6 +81,25 @@ public final class JsonValueSerializerRegistry
   public static JsonValueSerializerRegistry getInstance ()
   {
     return s_aInstance;
+  }
+
+  /**
+   * Register the value serializer for a certain class only known by name.
+   * 
+   * @param sClassName
+   *        The name of the class to be registered. May neither be
+   *        <code>null</code> nor empty.
+   * @param aValueSerializer
+   *        The value serializer. May not be <code>null</code>.
+   */
+  public void registerJsonValueSerializer (@Nonnull @Nonempty final String sClassName,
+                                           @Nonnull final IJsonValueSerializer aValueSerializer)
+  {
+    final Class <?> aClass = GenericReflection.getClassFromNameSafe (sClassName);
+    if (aClass == null)
+      s_aLogger.info ("Failed to resolve class '" + sClassName + "' so no JsonValueSerializer will be registered!");
+    else
+      registerJsonValueSerializer (aClass, aValueSerializer);
   }
 
   /**
