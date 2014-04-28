e45db366468f4802f60dc886e4b68f87f24c6e0a
ph <ph@phloc.com>
2014-04-28 07:50:56 +0000

    Using new parser for tests

--- phloc-json2/src/main/java/com/phloc/json2/parser/ParseUtils.java
+++ phloc-json2/src/main/java/com/phloc/json2/parser/ParseUtils.java
@@ -21,6 +21,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
 
 import com.phloc.commons.annotations.PresentForCodeCoverage;
+import com.phloc.commons.string.StringHelper;
 
 /**
  * This class is used by the generated parsers to do some common stuff.
@@ -37,9 +38,70 @@ public final class ParseUtils
   private ParseUtils ()
   {}
 
+  private static int _hexval (final char c)
+  {
+    final int ret = StringHelper.getHexValue (c);
+    if (ret < 0)
+      throw new IllegalArgumentException ("Illegal hex char '" + c + "'");
+    return ret;
+  }
+
+  @Nonnull
   public static String unescapeString (@Nonnull final StringBuilder aSB)
   {
-    final String sWithoutQuotes = aSB.substring (1, aSB.length () - 1);
-    return sWithoutQuotes;
+    final char [] aSrc = new char [aSB.length () - 2];
+    // Remove quotes
+    aSB.getChars (1, aSB.length () - 1, aSrc, 0);
+
+    if (aSB.indexOf ("\\") < 0)
+      return new String (aSrc);
+
+    final int nMax = aSrc.length;
+    final StringBuilder ret = new StringBuilder (nMax);
+    for (int i = 0; i < nMax; ++i)
+    {
+      final char c = aSrc[i];
+      if (c == '\\')
+      {
+        final char cNext = aSrc[++i];
+        switch (cNext)
+        {
+          case '"':
+          case '/':
+          case '\\':
+            ret.append (cNext);
+            break;
+          case 'b':
+            ret.append ('\b');
+            break;
+          case 'f':
+            ret.append ('\f');
+            break;
+          case 'n':
+            ret.append ('\n');
+            break;
+          case 'r':
+            ret.append ('\r');
+            break;
+          case 't':
+            ret.append ('\t');
+            break;
+          case 'u':
+          {
+            final char cU1 = aSrc[++i];
+            final char cU2 = aSrc[++i];
+            final char cU3 = aSrc[++i];
+            final char cU4 = aSrc[++i];
+            ret.append ((char) (_hexval (cU1) << 12 | _hexval (cU2) << 8 | _hexval (cU3) << 4 | _hexval (cU4)));
+            break;
+          }
+          default:
+            throw new IllegalArgumentException ("Unexpected escape sequence: \\" + cNext);
+        }
+      }
+      else
+        ret.append (c);
+    }
+    return ret.toString ();
   }
 }
