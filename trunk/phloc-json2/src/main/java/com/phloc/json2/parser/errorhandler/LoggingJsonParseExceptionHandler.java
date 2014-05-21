5c796900a6c9fd860c51de43bbf8d1be223c8e2b
ph@phloc.com <ph@phloc.com@82cfae7e-88cd-3e95-59e0-9798f0db0524>
2014-05-21 22:10:14 +0000

    Removed error handler

--- trunk/phloc-json2/src/main/java/com/phloc/json2/parser/errorhandler/LoggingJsonParseExceptionHandler.java
+++ trunk/phloc-json2/src/main/java/com/phloc/json2/parser/errorhandler/LoggingJsonParseExceptionHandler.java
@@ -18,12 +18,15 @@
 package com.phloc.json2.parser.errorhandler;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.phloc.commons.annotations.Nonempty;
 import com.phloc.json2.parser.ParseException;
+import com.phloc.json2.parser.Token;
 
 /**
  * An implementation of {@link IJsonParseExceptionHandler} that logs all
@@ -35,12 +38,90 @@ import com.phloc.json2.parser.ParseException;
 public class LoggingJsonParseExceptionHandler implements IJsonParseExceptionHandler
 {
   private static final Logger s_aLogger = LoggerFactory.getLogger (LoggingJsonParseExceptionHandler.class);
+  private static final int TOKEN_EOF = 0;
 
   public LoggingJsonParseExceptionHandler ()
   {}
 
+  @Nonnull
+  @Nonempty
+  public static String createLoggingStringParseError (@Nonnull final ParseException ex)
+  {
+    if (ex.currentToken == null)
+    {
+      // Is null if the constructor with String only was used
+      return ex.getMessage ();
+    }
+    return createLoggingStringParseError (ex.currentToken, ex.expectedTokenSequences, ex.tokenImage, null);
+  }
+
+  @Nonnull
+  @Nonempty
+  public static String createLoggingStringParseError (@Nonnull final Token aLastValidToken,
+                                                      @Nonnull final int [][] aExpectedTokenSequencesVal,
+                                                      @Nonnull final String [] aTokenImageVal,
+                                                      @Nullable final Token aLastSkippedToken)
+  {
+    if (aLastValidToken == null)
+      throw new NullPointerException ("LastValidToken");
+    if (aExpectedTokenSequencesVal == null)
+      throw new NullPointerException ("ExpectedTokenSequencesVal");
+    if (aTokenImageVal == null)
+      throw new NullPointerException ("TokenImageVal");
+
+    final StringBuilder aExpected = new StringBuilder ();
+    int nMaxSize = 0;
+    for (final int [] aExpectedTokens : aExpectedTokenSequencesVal)
+    {
+      if (nMaxSize < aExpectedTokens.length)
+        nMaxSize = aExpectedTokens.length;
+
+      if (aExpected.length () > 0)
+        aExpected.append (',');
+      for (final int nExpectedToken : aExpectedTokens)
+        aExpected.append (' ').append (aTokenImageVal[nExpectedToken]);
+    }
+
+    final StringBuilder retval = new StringBuilder (1024);
+    retval.append ('[')
+          .append (aLastValidToken.next.beginLine)
+          .append (':')
+          .append (aLastValidToken.next.beginColumn)
+          .append (']');
+    if (aLastSkippedToken != null)
+    {
+      retval.append ("-[")
+            .append (aLastSkippedToken.endLine)
+            .append (':')
+            .append (aLastSkippedToken.endColumn)
+            .append (']');
+    }
+    retval.append (" Encountered");
+    Token aCurToken = aLastValidToken.next;
+    for (int i = 0; i < nMaxSize; i++)
+    {
+      retval.append (' ');
+      if (aCurToken.kind == TOKEN_EOF)
+      {
+        retval.append (aTokenImageVal[TOKEN_EOF]);
+        break;
+      }
+      retval.append ("text '")
+            .append (aCurToken.image)
+            .append ("' corresponding to token ")
+            .append (aTokenImageVal[aCurToken.kind]);
+      aCurToken = aCurToken.next;
+    }
+    retval.append (". ");
+    if (aLastSkippedToken != null)
+      retval.append ("Skipped until token ").append (aLastSkippedToken).append (". ");
+    retval.append (aExpectedTokenSequencesVal.length == 1 ? "Was expecting:" : "Was expecting one of:")
+          .append (aExpected);
+    return retval.toString ();
+  }
+
   public void onException (@Nonnull final ParseException ex)
   {
-    s_aLogger.error ("Failed to parse CSS: " + LoggingJsonParseErrorHandler.createLoggingStringParseError (ex));
+    s_aLogger.error ("Failed to parse JSON: " + createLoggingStringParseError (ex));
   }
 }
